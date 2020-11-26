2463017963a5af28958c2e2b7dd64ec434c15e2b
KingAJ19 <anelesokwane@gmail>
2020-11-26 16:00:06 +0200

    About Page Layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container >
+    <br>
+    <br>
+    <v-row no-gutters>
+      <v-col>
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repellat libero eveniet tenetur quibusdam cupiditate earum eaque dolorem hic voluptas. Omnis assumenda nemo quasi, inventore atque vitae cupiditate saepe eaque?
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sapiente velit reprehenderit saepe nesciunt. Suscipit eius nulla, exercitationem obcaecati, provident doloremque voluptates cumque facilis beatae, laudantium adipisci quod sapiente quibusdam?
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur aliquid quos est rerum rem ullam sunt provident sit, alias, aspernatur consectetur veniam. Doloremque laudantium eum in beatae error, magnam quis!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore illo, voluptates animi labore modi fugiat doloremque suscipit beatae, praesentium esse, quasi similique eos? Inventore similique officiis impedit voluptates saepe minima.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, cupiditate voluptatem eius voluptas quibusdam illum tenetur ad error natus reiciendis expedita sit id nulla earum necessitatibus explicabo aliquam vel. Perspiciatis.
+        </v-card>
+      </v-col>
+      <v-col order="12">
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repellat libero eveniet tenetur quibusdam cupiditate earum eaque dolorem hic voluptas. Omnis assumenda nemo quasi, inventore atque vitae cupiditate saepe eaque?
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sapiente velit reprehenderit saepe nesciunt. Suscipit eius nulla, exercitationem obcaecati, provident doloremque voluptates cumque facilis beatae, laudantium adipisci quod sapiente quibusdam?
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur aliquid quos est rerum rem ullam sunt provident sit, alias, aspernatur consectetur veniam. Doloremque laudantium eum in beatae error, magnam quis!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore illo, voluptates animi labore modi fugiat doloremque suscipit beatae, praesentium esse, quasi similique eos? Inventore similique officiis impedit voluptates saepe minima.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, cupiditate voluptatem eius voluptas quibusdam illum tenetur ad error natus reiciendis expedita sit id nulla earum necessitatibus explicabo aliquam vel. Perspiciatis.
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 <script>
 export default {
   name: "About"
